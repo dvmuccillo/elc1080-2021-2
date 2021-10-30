406e2b8e5ee6a0ab8755a760312ed13a221ff2cb
Daniel Vargas Muccillo <daniel.vargasmuccillo@gmail.com>
2021-10-30 14:39:30 -0300

    mem basic

--- trabalhos/src/memory/Memory.java
+++ trabalhos/src/memory/Memory.java
@@ -2,4 +2,22 @@ package memory;
 
 public class Memory {
     
+    private Integer size;
+
+    public Memory(Integer size){
+        this.size = size;
+    }
+
+    public Error read(Integer address, Integer value){
+        return Error.OK;
+    }
+
+    private Integer getSize(){
+        return this.size;
+    }
+
+    public Error write(Integer address, Integer value){
+        return Error.OK;
+    }
+
 }
