8bc6dfed92fc7006ed89473908fc7b2c83b70561
Daniel Vargas Muccillo <daniel.vargasmuccillo@gmail.com>
2021-10-30 14:15:42 -0300

    pacote pra memória

---
+++ trabalhos/src/memory/Error.java
@@ -0,0 +1,18 @@
+package memory;
+
+public enum Error {
+    
+    OK(0),
+    INVALID_ADDRESS(1);
+
+    private final Integer value;
+
+    private Error(Integer value) {
+        this.value = value;
+    }
+
+    public Integer getValue(){
+        return this.value;
+    }
+
+}
