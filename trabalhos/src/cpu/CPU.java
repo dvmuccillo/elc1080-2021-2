132f0db16617f3fa422a9e60568462a93618903a
Daniel Vargas Muccillo <daniel.vargasmuccillo@gmail.com>
2021-11-01 15:53:17 -0300

    cpu 0

---
+++ trabalhos/src/cpu/CPU.java
@@ -0,0 +1,38 @@
+package cpu;
+
+public class CPU {
+    
+    /**
+     * Contador de programa.
+     * Registrador que contém o endereço da memória onde se encontra a próxima instrução a ser executada.
+     */
+    private int PC;
+
+    /**
+     * Acumulador.
+     * Registrador que contém um dado usado de forma implícita em várias instruções.
+     */
+    private int A;
+
+    /**
+     * Registrador auxiliar.
+     */
+    private int X;
+
+    /**
+     * Modo da CPU.
+     * Registrador que contém o modo de execução da CPU, pode ser:
+     * - normal - (valor 0) a CPU está apta a executar a próxima instrução;
+     * - parada - (valor diferente de 0) a CPU não pode executar a próxima instrução.
+     *            O código identifica o motivo (execução suspensa por ter executado uma instrução de parada,
+     *            ou erro causado na execução da última instrução).
+     */
+    private int mode;
+
+    /**
+     * Complemento de Parada.
+     * Registrador que contém informação complementar sobre o motivo de não estar em modo normal.
+     */
+    private int stopInfo;
+
+}
---
+++ trabalhos/src/cpu/Error.java
@@ -0,0 +1,17 @@
+package cpu;
+
+public enum Error {
+    OK(0),
+    STOPED(1),
+    INVALID_INSTRUCTION(2);
+
+    private final Integer value;
+
+    private Error(Integer value) {
+        this.value = value;
+    }
+
+    public Integer getValue(){
+        return this.value;
+    }
+}
